19d21c6713787220fcaddd5b55c88cb364555819
NEVEROV iaroslav <beeguy74@gmail.com>
2024-08-03 18:17:52 +0200

    download working

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,57 @@
+use std::io::copy;
+
 use reqwest;
-use scraper::{Html, Selector};
+use regex::Regex;
+
+fn url_extension_searcher(html: &str, extension: &str) -> Vec<String> {
+    let re = Regex::new(&format!(r#"(https?://[^\s]*?\.{}[^\s]*?)"#, extension)).unwrap();
+    let mut res = Vec::new();
+    for cap in re.captures_iter(html) {
+        res.push(cap[1].to_string());
+    }
+    res
+}
+
+async fn download_to_file(url: &str, dir_path: &str) {
+    let filename = dir_path.to_string() + "/" + url.split("/").last().unwrap();
+    let mut file = std::fs::File::create(filename).unwrap();
+    let mut resp = reqwest::get(url).await;
+    if resp.is_err() {
+        println!("Failed to download {}", url);
+        return;
+    }
+    else {
+        copy(&mut resp.unwrap().bytes().await.unwrap().as_ref(), &mut file).unwrap();
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = "https://www.osnews.com";
     let resp = reqwest::get(url).await?.text().await?;
-    let fragment = Html::parse_fragment(&resp);
-    let article_selector = Selector::parse("div#content_box article").unwrap();
-    let title_selector = Selector::parse("h1.title a").unwrap();
-    let author_selector = Selector::parse("span.theauthor a").unwrap();
-    let date_selector = Selector::parse("span.thetime").unwrap();
+    let jpgs: Vec<String> = url_extension_searcher(&resp, "jpg");
+    let pngs = url_extension_searcher(&resp, "png");
+    let jpegs: Vec<String> = url_extension_searcher(&resp, "jpeg");
+    let gifs: Vec<String> = url_extension_searcher(&resp, "gif");
+    let bmps: Vec<String> = url_extension_searcher(&resp, "bmp");
 
-    for (i, article) in fragment.select(&article_selector).enumerate() {
-        if i >= 5 {
-            break;
-        }
 
-        let title = article.select(&title_selector).next().unwrap().inner_html();
-        let author = article
-            .select(&author_selector)
-            .next()
-            .unwrap()
-            .inner_html();
-        let date = article
-            .select(&date_selector)
-            .next()
-            .unwrap()
-            .text()
-            .collect::<Vec<_>>()
-            .join("");
-        let url = article
-            .select(&title_selector)
-            .next()
-            .unwrap()
-            .value()
-            .attr("href")
-            .unwrap();
 
-        println!("Article Title: {}", title);
-        println!("Posted by: {}", author);
-        println!("Posted on: {}", date);
-        println!("Read more: {}", url);
-        println!("_____");
+    for png in pngs {
+        println!("{}", png);
+        download_to_file(&png, "./images").await;
+    }
+    for jpg in jpgs {
+        println!("{}", jpg);
+    }
+    for jpeg in jpegs {
+        println!("{}", jpeg);
+    }
+    for gif in gifs {
+        println!("{}", gif);
+    }
+    for bmp in bmps {
+        println!("{}", bmp);
     }
 
     Ok(())
