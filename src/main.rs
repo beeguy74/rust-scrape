f336b50b7e97a151a9dd473f8430f567373c60e2
Iaroslav NEVEROV <beeguy74@gmail.com>
2024-08-17 17:58:55 +0200

    some troubles

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 use std::io::copy;
+use std::env;
 
 use reqwest;
 use regex::Regex;
+
+struct Args {
+    url: String,
+    dir_path: String,
+    deep: i32,
+}
+
+impl Args {
+    fn default() -> Self {
+        Args {
+            url: "https://www.osnews.com".to_string(),
+            dir_path: "./data".to_string(),
+            deep: 5,
+        }
+    }
+}
 
 fn url_extension_searcher(html: &str, extension: &str) -> Vec<String> {
     let re = Regex::new(&format!(r#"(https?://[^\s]*?\.{}[^\s]*?)"#, extension)).unwrap();
@@ -25,12 +42,39 @@ async fn download_to_file(url: &str, dir_path: &str) {
     }
 }
 
+fn parse_args() -> Args {
+    let mut args = Args::default();
+    let mut i = 1;
+    while i < env::args().len() {
+        match env::args().nth(i).unwrap().as_str() {
+            "-r" => {
+                args.url = env::args().nth(i + 1).unwrap();
+                i += 2;
+            }
+            "-p" => {
+                args.dir_path = env::args().nth(i + 1).unwrap();
+                i += 2;
+            }
+            "-l" => {
+                args.deep = env::args().nth(i + 1).unwrap().parse::<i32>().unwrap();
+                i += 2;
+            }
+            _ => {
+                i += 1;
+            }
+        }
+    }
+    args
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = "https://www.osnews.com";
-    let resp = reqwest::get(url).await?.text().await?;
+    let args: Args = parse_args();
+    println!("url: {}, dir_path: {}, deep: {}", args.url, args.dir_path, args.deep);
+    let resp = reqwest::get(args.url).await?.text().await?;
+
     let jpgs: Vec<String> = url_extension_searcher(&resp, "jpg");
-    let pngs = url_extension_searcher(&resp, "png");
+    let pngs: Vec<String> = url_extension_searcher(&resp, "png");
     let jpegs: Vec<String> = url_extension_searcher(&resp, "jpeg");
     let gifs: Vec<String> = url_extension_searcher(&resp, "gif");
     let bmps: Vec<String> = url_extension_searcher(&resp, "bmp");
@@ -39,7 +83,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     for png in pngs {
         println!("{}", png);
-        download_to_file(&png, "./images").await;
+        download_to_file(&png, args.).await;
     }
     for jpg in jpgs {
         println!("{}", jpg);
