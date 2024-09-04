7cc4e839918bf4bdcedeb9f683bb3bb240dcc412
Iaroslav NEVEROV <beeguy74@gmail.com>
2024-09-04 12:59:55 +0200

    launcher matches extension and magic number

--- src/bin/scorpion.rs
+++ src/bin/scorpion.rs
@@ -28,18 +28,53 @@ fn open_file(file_path: &str) -> Result<imageFile, Box<dyn std::error::Error>> {
     Ok(result)
 }
 
+fn launcher(image: &mut imageFile) {
+    let hex_content = hex::encode(&image.content);
+    let magic_number = &hex_content[0..8];
+    println!("Magic Number: {}", magic_number);
+    match image.extension.as_str() {
+        "png" => {
+            if magic_number == "89504e47" {
+                println!("File is a PNG");
+            } else {
+                println!("File is not a PNG");
+            }
+        }
+        "jpg" | "jpeg" => {
+            if magic_number == "ffd8ffe0" {
+                println!("File is a JPEG");
+            } else {
+                println!("File is not a JPEG");
+            }
+        }
+        "gif" => {
+            if magic_number == "47494638" {
+                println!("File is a GIF");
+            } else {
+                println!("File is not a GIF");
+            }
+        }
+        "bmp" => {
+            if magic_number == "424d" {
+                println!("File is a BMP");
+            } else {
+                println!("File is not a BMP");
+            }
+        }
+        _ => {
+            println!("File extension not supported");
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = env::args();
     if args.len() != 2 {
         println!("Usage: {} <file>", args.nth(0).unwrap());
         return Err("Invalid number of arguments".into());
     }
-    let image = open_file(args.nth(1).unwrap().as_str())?;
-    // print first line of file
-    println!("Content: {:?}", &image.content[0..8]);
-    let hex_content = hex::encode(&image.content);
-    // Print the hexadecimal content
-    println!("Hex Content: {}", hex_content);
+    let mut image = open_file(args.nth(1).unwrap().as_str())?;
+    launcher(&mut image);
 
     Ok(())
 }
