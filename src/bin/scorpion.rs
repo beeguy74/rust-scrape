e4f0b7bb6dc5c8ee5125ff3c201e5759a7fb92f3
Iaroslav NEVEROV <beeguy74@gmail.com>
2024-09-04 12:31:30 +0200

    image struct

--- src/bin/scorpion.rs
+++ src/bin/scorpion.rs
@@ -1,24 +1,43 @@
-use std::{env, io::Read};
+use std::{env, io::Read, result};
 use hex;
 
-fn open_file(file_path: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+
+struct imageFile {
+    content: Vec<u8>,
+    extension: String,
+}
+
+impl imageFile {
+    fn len(&self) -> usize {
+        self.content.len()
+    }
+
+    fn set_content(&mut self, buf: &mut std::io::BufReader<std::fs::File>) {
+        buf.read_to_end(&mut self.content);
+    }
+}
+
+fn open_file(file_path: &str) -> Result<imageFile, Box<dyn std::error::Error>> {
     let file = std::fs::File::open(file_path)?;
     let mut buf_reader = std::io::BufReader::new(file);
-    let mut content = Vec::new();
-    buf_reader.read_to_end(&mut content)?;
-    Ok(content)
+    let mut result = imageFile {
+        content: Vec::new(),
+        extension: file_path.split('.').last().unwrap().to_string(),
+    };
+    result.set_content(&mut buf_reader);
+    Ok(result)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = env::args();
     if args.len() != 2 {
         println!("Usage: {} <file>", args.nth(0).unwrap());
-        return Ok(());
+        return Err("Invalid number of arguments".into());
     }
-    let content = open_file(args.nth(1).unwrap().as_str())?;
+    let image = open_file(args.nth(1).unwrap().as_str())?;
     // print first line of file
-    println!("Content: {:?}", &content[0..8]);
-    let hex_content = hex::encode(&content);
+    println!("Content: {:?}", &image.content[0..8]);
+    let hex_content = hex::encode(&image.content);
     // Print the hexadecimal content
     println!("Hex Content: {}", hex_content);
 
